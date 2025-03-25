02c2c505d4bf64071a6f64f5baa75c69bd4659e8
Sotnikov Oleksandr <ssotnikovvv55@gmail.com>
2025-03-25 18:47:16 +0200

    Solution Design by SOTNIKOV

--- taxi/views.py
+++ taxi/views.py
@@ -1,15 +1,46 @@
-from django.shortcuts import render
+from django.views import generic
 
 from taxi.models import Driver, Car, Manufacturer
 
 
-def index(request):
-    """View function for the home page of the site."""
+class ManufacturerListView(generic.ListView):
+    model = Manufacturer
+    queryset = Manufacturer.objects.all().order_by("name")
+    paginate_by = 5
+    template_name = "taxi/manufacturer_list.html"
 
-    context = {
-        "num_drivers": Driver.objects.count(),
-        "num_cars": Car.objects.count(),
-        "num_manufacturers": Manufacturer.objects.count(),
-    }
 
-    return render(request, "taxi/index.html", context=context)
+class CarListView(generic.ListView):
+    model = Car
+    queryset = Car.objects.select_related("manufacturer")
+    paginate_by = 5
+    template_name = "taxi/car_list.html"
+
+
+class CarDetailView(generic.ListView):
+    model = Car
+    template_name = "taxi/car_detail.html"
+
+
+class DriverListView(generic.ListView):
+    model = Driver
+    paginate_by = 5
+    template_name = "taxi/driver_list.html"
+
+
+class DriverDetailView(generic.ListView):
+    model = Driver
+    template_name = "taxi/driver_detail.html"
+
+
+class IndexView(generic.TemplateView):
+    template_name = "taxi/index.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context.update({
+            "num_drivers": Driver.objects.count(),
+            "num_cars": Car.objects.count(),
+            "num_manufacturers": Manufacturer.objects.count(),
+        })
+        return context
